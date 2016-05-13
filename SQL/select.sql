085239fc0dd08fa323277fa12eaf9180adf692f3
halleyok <jonas_leaozinho@hotmail.com>
2016-05-13 17:40:38 +0100

    Add files via upload

---
+++ SQL/select.sql
@@ -0,0 +1,43 @@
+.bail ON
+.mode columns
+.headers on
+.nullvalue NULL
+PRAGMA foreign_keys = ON;
+
+
+
+--Lista dos seguros ordenados do mais caro para o mais barato
+
+
+
+
+
+--Lista dos seguros com o seguro mais caro
+
+
+
+
+--Lista clientes com contrato acabar dentro de 6 meses
+
+--Lista dos veiculos com mais extras
+
+
+--Lista dos clientes que tiveram culpa em acidentes
+
+--Lista dos seguros ordenados por pagamento do maior para o menor
+
+--Lista o valor medio de cada tipo de acidente
+--Lista os clientes com mais mais acidentes
+--Lista dos clientes com mais acidentes em que e culpa 
+
+--Custo medio dos acidentes de um cliente, independente se e ou nao culpado
+
+--Lista de veiculos de cada Cliente
+
+--Lista de contratos de cada cliente
+
+--Lista da media dos precos dos seguros
+
+--Lista dos numero de contratos por marca
+
+--Qual a matricula do carro com mais contratos
